2c968d1ad8040751365d4205cd434499bde36f5a
Yvette Demidova <yvette_demidova@mail.ru>
2023-01-17 15:35:18 +0300

    Добавлен раппер в футер

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,119 +11,121 @@ import FooterStyles from './Footer.module.css'
 function Footer() {
   return (
     <div className={FooterStyles.Footer}>
-      <div className={FooterStyles.column}>
-        <div className={FooterStyles.Logo}>
+      <div className={FooterStyles.wr}>
+        <div className={FooterStyles.column}>
+          <div className={FooterStyles.Logo}>
+            <Link
+              to="/"
+              className={FooterStyles.Link}
+            >
+              <img
+                src={logoWithLetters}
+                alt=""
+              />
+            </Link>
+          </div>
+          <div className={classNames(FooterStyles.Secondary, FooterStyles.smaller)}>
+            &copy; &laquo;Интернет-магазин DogFood.ru&raquo;
+          </div>
+        </div>
+        <div className={FooterStyles.column}>
+          <Link
+            to="./products"
+            className={classNames(FooterStyles.Link, FooterStyles.smaller)}
+          >
+            Каталог
+          </Link>
           <Link
             to="/"
-            className={FooterStyles.Link}
+            className={classNames(FooterStyles.Link, FooterStyles.smaller)}
           >
-            <img
-              src={logoWithLetters}
-              alt=""
-            />
+            Акции
+          </Link>
+          <Link
+            to="/"
+            className={classNames(FooterStyles.Link, FooterStyles.smaller)}
+          >
+            Новости
+          </Link>
+          <Link
+            to="/"
+            className={classNames(FooterStyles.Link, FooterStyles.smaller)}
+          >
+            Отзывы
           </Link>
         </div>
-        <div className={classNames(FooterStyles.Secondary, FooterStyles.smaller)}>
-          &copy; &laquo;Интернет-магазин DogFood.ru&raquo;
+        <div className={FooterStyles.column}>
+          <Link
+            to="/"
+            className={classNames(FooterStyles.Link, FooterStyles.smaller)}
+          >
+            Оплата и доставка
+          </Link>
+          <Link
+            to="/"
+            className={classNames(FooterStyles.Link, FooterStyles.smaller)}
+          >
+            Часто спрашивают
+          </Link>
+          <Link
+            to="/"
+            className={classNames(FooterStyles.Link, FooterStyles.smaller)}
+          >
+            Обратная связь
+          </Link>
+          <Link
+            to="/"
+            className={classNames(FooterStyles.Link, FooterStyles.smaller)}
+          >
+            Контакты
+          </Link>
         </div>
-      </div>
-      <div className={FooterStyles.column}>
-        <Link
-          to="./products"
-          className={classNames(FooterStyles.Link, FooterStyles.smaller)}
-        >
-          Каталог
-        </Link>
-        <Link
-          to="/"
-          className={classNames(FooterStyles.Link, FooterStyles.smaller)}
-        >
-          Акции
-        </Link>
-        <Link
-          to="/"
-          className={classNames(FooterStyles.Link, FooterStyles.smaller)}
-        >
-          Новости
-        </Link>
-        <Link
-          to="/"
-          className={classNames(FooterStyles.Link, FooterStyles.smaller)}
-        >
-          Отзывы
-        </Link>
-      </div>
-      <div className={FooterStyles.column}>
-        <Link
-          to="/"
-          className={classNames(FooterStyles.Link, FooterStyles.smaller)}
-        >
-          Оплата и доставка
-        </Link>
-        <Link
-          to="/"
-          className={classNames(FooterStyles.Link, FooterStyles.smaller)}
-        >
-          Часто спрашивают
-        </Link>
-        <Link
-          to="/"
-          className={classNames(FooterStyles.Link, FooterStyles.smaller)}
-        >
-          Обратная связь
-        </Link>
-        <Link
-          to="/"
-          className={classNames(FooterStyles.Link, FooterStyles.smaller)}
-        >
-          Контакты
-        </Link>
-      </div>
-      <div className={FooterStyles.column}>
-        <h4>Мы на связи</h4>
-        <div className={FooterStyles.ContactsWr}>
-          <h5>8(999)000-00-00</h5>
-          <p className={classNames(FooterStyles.smaller)}>
-            <FontAwesomeIcon icon={faEnvelope} className={classNames(FooterStyles.icon)} />
-            <a
-              href="mailto:dogfood.ru@gmail.com"
+        <div className={FooterStyles.column}>
+          <h4>Мы на связи</h4>
+          <div className={FooterStyles.ContactsWr}>
+            <h5>8(999)000-00-00</h5>
+            <p className={classNames(FooterStyles.smaller)}>
+              <FontAwesomeIcon icon={faEnvelope} className={classNames(FooterStyles.icon)} />
+              <a
+                href="mailto:dogfood.ru@gmail.com"
+                className={classNames(FooterStyles.Link)}
+              >
+                dogfood.ru@gmail.com
+              </a>
+            </p>
+          </div>
+          <div className={FooterStyles.BtnWr}>
+            <Link
+              to="/"
               className={classNames(FooterStyles.Link)}
             >
-              dogfood.ru@gmail.com
-            </a>
-          </p>
-        </div>
-        <div className={FooterStyles.BtnWr}>
-          <Link
-            to="/"
-            className={classNames(FooterStyles.Link)}
-          >
-            <FontAwesomeIcon icon={faTelegram} />
-          </Link>
-          <Link
-            to="/"
-            className={classNames(FooterStyles.Link)}
-          >
-            <FontAwesomeIcon icon={faWhatsapp} />
-          </Link>
-          <Link
-            to="/"
-            className={classNames(FooterStyles.Link)}
-          >
-            <FontAwesomeIcon icon={faViber} />
-          </Link>
-          <Link
-            to="/"
-            className={classNames(FooterStyles.Link)}
-          >
-            <FontAwesomeIcon icon={faInstagram} />
-          </Link>
-          <Link
-            to="/"
-            className={classNames(FooterStyles.Link)}
-          >
-            <FontAwesomeIcon icon={faVk} />
-          </Link>
+              <FontAwesomeIcon icon={faTelegram} />
+            </Link>
+            <Link
+              to="/"
+              className={classNames(FooterStyles.Link)}
+            >
+              <FontAwesomeIcon icon={faWhatsapp} />
+            </Link>
+            <Link
+              to="/"
+              className={classNames(FooterStyles.Link)}
+            >
+              <FontAwesomeIcon icon={faViber} />
+            </Link>
+            <Link
+              to="/"
+              className={classNames(FooterStyles.Link)}
+            >
+              <FontAwesomeIcon icon={faInstagram} />
+            </Link>
+            <Link
+              to="/"
+              className={classNames(FooterStyles.Link)}
+            >
+              <FontAwesomeIcon icon={faVk} />
+            </Link>
+          </div>
         </div>
       </div>
     </div>
