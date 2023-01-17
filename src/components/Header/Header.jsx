490a3fc4872dc4f90acd757160dc3a314ab6f10b
Yvette Demidova <yvette_demidova@mail.ru>
2023-01-17 15:30:10 +0300

    Добавлены иконки в шапку

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,7 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartShopping, faPaw } from '@fortawesome/free-solid-svg-icons'
+import { faHeart } from '@fortawesome/free-regular-svg-icons/faHeart'
+import classNames from 'classnames'
 import { Link } from 'react-router-dom'
 import logo from '../../images/logo-husky-black.svg'
 import logoWithLetters from '../../images/logo-with-letters-black.svg'
@@ -5,25 +9,77 @@ import Search from '../Search/Search'
 import headerStyles from './Header.module.css'
 
 const vw = window.innerWidth
+const auth = true
 function Header() {
   console.log(vw)
+  let btns = (
+    <>
+      <button
+        type="button"
+        className={headerStyles.Button}
+      >
+        <Link
+          to="./signin"
+          className={headerStyles.Link}
+        >
+          Войти
+        </Link>
+      </button>
+      <button
+        type="button"
+        className={headerStyles.Button}
+      >
+        <Link
+          to="./signup"
+          className={headerStyles.Link}
+        >
+          Зарегистрироваться
+        </Link>
+      </button>
+    </>
+  )
+  if (auth) {
+    btns = (
+      <>
+        <Link
+          to="/"
+          className={classNames(headerStyles.Link)}
+        >
+          <FontAwesomeIcon icon={faHeart} />
+        </Link>
+        <Link
+          to="/"
+          className={classNames(headerStyles.Link)}
+        >
+          <FontAwesomeIcon icon={faCartShopping} />
+        </Link>
+        <Link
+          to="/"
+          className={classNames(headerStyles.Link)}
+        >
+          <FontAwesomeIcon icon={faPaw} />
+        </Link>
+      </>
+    )
+  }
   return (
     <div className={headerStyles.Header}>
-      <div className={headerStyles.Logo}>
-        <Link to="/" className={headerStyles.Link}>
-          <img src={vw > 768 ? logoWithLetters : logo} alt="" />
-        </Link>
-      </div>
-      <div className={headerStyles.Search}>
-        <Search />
-      </div>
-      <div className={headerStyles.Buttons}>
-        <button type="button" className={headerStyles.Button}>
-          <Link to="./signin" className={headerStyles.Link}>Войти</Link>
-        </button>
-        <button type="button" className={headerStyles.Button}>
-          <Link to="./signup" className={headerStyles.Link}>Зарегистрироваться</Link>
-        </button>
+      <div className={headerStyles.wr}>
+        <div className={headerStyles.Logo}>
+          <Link
+            to="/"
+            className={headerStyles.Link}
+          >
+            <img
+              src={vw > 768 ? logoWithLetters : logo}
+              alt=""
+            />
+          </Link>
+        </div>
+        <div className={headerStyles.Search}>
+          <Search />
+        </div>
+        <div className={headerStyles.Buttons}>{btns}</div>
       </div>
     </div>
   )
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -2,9 +2,7 @@ import SearchStyles from './Search.module.css'
 
 function Search() {
   return (
-    <div className={SearchStyles.Search}>
-      <input type="search" name="q" placeholder="Искать..." className={SearchStyles.Search_Field} />
-    </div>
+    <input type="search" name="q" placeholder="Искать..." className={SearchStyles.Search_Field} />
   )
 }
 
